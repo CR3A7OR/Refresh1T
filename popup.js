ecb0f937cd98a482f12376a829b8aacb3c18a354
CR3A7OR <67477209+CR3A7OR@users.noreply.github.com>
2022-03-23 03:31:22 +0000

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,7 +2,18 @@
 document.getElementById('start').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
         // WAY 2
-        chrome.tabs.sendMessage(activeTabs[0].id, { action: 'start' });
+        // chrome.tabs.sendMessage(activeTabs[0].id, { action: 'start' });
+		chrome.storage.local.set({ action: 'start' });
+
+    });
+});
+
+document.getElementById('stop').addEventListener('click', function() {
+    chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+        // WAY 2
+        // chrome.tabs.sendMessage(activeTabs[0].id, { action: 'start' });
+		chrome.storage.local.set({ action: 'stop' });
+
     });
 });
 // https://stackoverflow.com/questions/38561136/chrome-extension-to-change-dom-with-a-button-in-extension-popup
