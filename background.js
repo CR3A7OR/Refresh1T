e151c34221b6635868e5a7b1978dfd2ec8ef399d
CR3A7OR <67477209+CR3A7OR@users.noreply.github.com>
2022-06-07 19:28:46 +0100

    Version 1.3.1

--- background.js
+++ background.js
@@ -4,29 +4,3 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
         sendResponse({tab: sender.tab.id});
      }
 });
-
-/* OLD METHOD CHECKING VALUE AGAINST SAVED PREVIOUS
-let preHash = '';
-chrome.runtime.onMessage.addListener((msg, sender, response) => {
-    if (msg.from === 'content'){
-        console.log(msg.text);
-        console.log(sender.tab.id);
-
-        if (msg.text === sender.tab.id) {
-            if (preHash === '') { preHash = msg.subject; response("true"); return true;}
-            if (msg.subject == preHash){
-                console.log(msg.subject);
-                response("true");
-                return true;
-            }
-            else {
-                response("false");
-                return true;
-            }
-        }else {
-            response("false");
-            return true;
-        }
-    }
-});
-*/
