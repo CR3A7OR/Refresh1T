f295fe40273d98c1de9397513111a62554d41a31
CR3A7OR <67477209+CR3A7OR@users.noreply.github.com>
2022-06-07 19:26:48 +0100

    Version 1.3

--- background.js
+++ background.js
@@ -1,16 +1,32 @@
-let preHash = '';
+/* RETURNS THE TAB ID OF THE TAB SENDING MESSAGE*/
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    if (msg.text == "getID") {
+        sendResponse({tab: sender.tab.id});
+     }
+});
 
+/* OLD METHOD CHECKING VALUE AGAINST SAVED PREVIOUS
+let preHash = '';
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
     if (msg.from === 'content'){
-        if (preHash === '') { preHash = msg.subject; response("true"); return true;}
-        if (msg.subject == preHash){
-            console.log(msg.subject);
-            response("true");
-            return true;
-        }
-        else {
+        console.log(msg.text);
+        console.log(sender.tab.id);
+
+        if (msg.text === sender.tab.id) {
+            if (preHash === '') { preHash = msg.subject; response("true"); return true;}
+            if (msg.subject == preHash){
+                console.log(msg.subject);
+                response("true");
+                return true;
+            }
+            else {
+                response("false");
+                return true;
+            }
+        }else {
             response("false");
             return true;
         }
     }
 });
+*/
