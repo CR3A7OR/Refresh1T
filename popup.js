be0f19dc24e1198941ad61987c6d87cfd4fc5c3b
CR3A7OR <67477209+CR3A7OR@users.noreply.github.com>
2022-06-07 19:26:20 +0100

    Version 1.3

--- popup.js
+++ popup.js
@@ -1,19 +1,19 @@
 // On start click, start content.js
 document.getElementById('start').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-        // WAY 2
-        // chrome.tabs.sendMessage(activeTabs[0].id, { action: 'start' });
 		chrome.storage.local.set({ action: 'start' });
+    });
 
+    /* GETS TAB ID OF CURRENT TAB  */
+    chrome.tabs.query({ active: true, currentWindow: true}, function(tabs) {
+        var myTabId = tabs[0].id;
+        chrome.storage.local.set({ tabID: myTabId });
     });
 });
 
 document.getElementById('stop').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-        // WAY 2
-        // chrome.tabs.sendMessage(activeTabs[0].id, { action: 'start' });
 		chrome.storage.local.set({ action: 'stop' });
-
+        chrome.storage.local.clear();
     });
 });
-
