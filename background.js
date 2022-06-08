d443531d1745760b05b2bfe31fb8d66155e07f82
CR3A7OR <67477209+CR3A7OR@users.noreply.github.com>
2022-06-08 17:41:21 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
+/*
+ * @module background.js
+ * - returns tab id of requesting tab
+ * @author CR3AT0R
+ * @license MIT
+ */
+
 /* RETURNS THE TAB ID OF THE TAB SENDING MESSAGE*/
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text == "getID") {
